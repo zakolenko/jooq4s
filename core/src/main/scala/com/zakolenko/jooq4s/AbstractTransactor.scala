33eba4aaac11aa718f8ba5297117921703bb3a82
zakolenko <zakolenkoroman@gmail.com>
2019-11-11 20:44:39 +0200

    fix warnings

--- core/src/main/scala/com/zakolenko/jooq4s/AbstractTransactor.scala
+++ core/src/main/scala/com/zakolenko/jooq4s/AbstractTransactor.scala
@@ -2,7 +2,7 @@ package com.zakolenko.jooq4s
 
 import cats.effect.{Blocker, ContextShift, Resource, Sync}
 import org.jooq.scalaextensions.Conversions._
-import org.jooq.{DSLContext, Query, Record, ResultQuery}
+import org.jooq.{DSLContext, Query => JQuery, Record, ResultQuery => JResultQuery}
 
 import scala.collection.JavaConverters._
 
@@ -11,21 +11,21 @@ class AbstractTransactor[F[_]: Sync: ContextShift](
   blocker: Blocker
 ) extends Transactor[F] {
 
-  override def one[R <: Record](rq: ResultQuery[R]): F[R] = {
+  override def one[R <: Record](rq: JResultQuery[R]): F[R] = {
     withDslF(_.fetchOne(rq))
   }
 
-  override def option[R <: Record](rq: ResultQuery[R]): F[Option[R]] = {
+  override def option[R <: Record](rq: JResultQuery[R]): F[Option[R]] = {
     withDslF(_.fetchOneOption(rq))
   }
 
-  override def stream[R <: Record](rq: ResultQuery[R]): fs2.Stream[F, R] = {
+  override def stream[R <: Record](rq: JResultQuery[R]): fs2.Stream[F, R] = {
     fs2.Stream
       .resource(Resource.fromAutoCloseable(blocker.delay(dsl.fetchLazy(rq))))
       .flatMap(cursor => fs2.Stream.fromBlockingIterator(blocker, cursor.iterator.asScala))
   }
 
-  override def execute(query: Query): F[Int] = {
+  override def execute(query: JQuery): F[Int] = {
     withDslF(_.execute(query))
   }
 
