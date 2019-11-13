eebec4f210d864fcb3db2462f3b01912f2616be3
zakolenko <zakolenkoroman@gmail.com>
2019-11-14 01:06:06 +0200

    use limited transactor

--- hikari/src/main/scala/com/zakolenko/jooq4s/hikari/HikariTransactor.scala
+++ hikari/src/main/scala/com/zakolenko/jooq4s/hikari/HikariTransactor.scala
@@ -1,43 +1,54 @@
 package com.zakolenko.jooq4s.hikari
 
-import java.util.concurrent.{ExecutorService, Executors}
-
-import cats.effect.{Blocker, ContextShift, Resource, Sync}
-import com.zakolenko.jooq4s.Transactor
+import cats.effect.concurrent.Semaphore
+import cats.effect.{Blocker, Concurrent, ContextShift, Resource, Sync}
+import cats.implicits._
+import com.zakolenko.jooq4s.util.{ExecutionContexts, Resources}
+import com.zakolenko.jooq4s.{LimitedTransactor, Transactor}
 import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
 import org.jooq.SQLDialect
 
-import scala.concurrent.ExecutionContext
-
 object HikariTransactor {
 
-  def apply[F[_]: Sync: ContextShift](
+  private def apply[F[_]: Concurrent: ContextShift](
+    hikariDataSource: HikariDataSource,
+    dialect: SQLDialect,
+    blocker: Blocker,
+    limited: Boolean
+  ): F[Transactor[F]] = {
+    Semaphore
+      .apply[F](hikariDataSource.getMaximumPoolSize)
+      .map { semaphore =>
+        val transactor = Transactor.fromDataSource(hikariDataSource, dialect, blocker)
+        if (limited) new LimitedTransactor(transactor, semaphore)
+        else transactor
+      }
+  }
+
+  def apply[F[_]: Concurrent: ContextShift](
     hikariDataSource: HikariDataSource,
     dialect: SQLDialect,
     blocker: Blocker
-  ): Transactor[F] = {
-    Transactor.fromDataSource(hikariDataSource, dialect, blocker)
+  ): F[Transactor[F]] = {
+    HikariTransactor(hikariDataSource, dialect, blocker, limited = true)
   }
 
-  def apply[F[_]: Sync: ContextShift](
+  def apply[F[_]: Concurrent: ContextShift](
     hikariConfig: HikariConfig,
     dialect: SQLDialect,
     blocker: Blocker
   ): Resource[F, Transactor[F]] = {
-    val acquire: F[HikariDataSource] = Sync[F].delay(new HikariDataSource(hikariConfig))
-    val release: HikariDataSource => F[Unit] = hds => Sync[F].delay(hds.close())
-
-    Resource
-      .make(acquire)(release)
-      .map(HikariTransactor.apply(_, dialect, blocker))
+    Resources
+      .make(new HikariDataSource(hikariConfig))(_.close())
+      .evalMap(HikariTransactor.apply(_, dialect, blocker))
   }
 
-  def apply[F[_]: Sync: ContextShift](
+  def apply[F[_]: Concurrent: ContextShift](
     driverClassName: String,
     dialect: SQLDialect,
     jdbcUrl: String,
     username: String,
-    password: String,
+    password: String
   ): Resource[F, Transactor[F]] = {
     for {
       hikariConfig <- {
@@ -52,13 +63,17 @@ object HikariTransactor {
         })
       }
 
-      executionContext <- {
-        val acquire: F[ExecutorService] = Sync[F].delay(Executors.newFixedThreadPool(hikariConfig.getMaximumPoolSize))
-        val release: ExecutorService => F[Unit] = es => Sync[F].delay(es.shutdown())
-        Resource.make(acquire)(release).map(ExecutionContext.fromExecutor)
-      }
+      executionContext <- ExecutionContexts.fixed(hikariConfig.getMaximumPoolSize)
+      dataSource <- Resources.make(new HikariDataSource(hikariConfig))(_.close())
 
-      transactor <- HikariTransactor(hikariConfig, dialect, Blocker.liftExecutionContext(executionContext))
+      transactor <- {
+        Resource.liftF(HikariTransactor.apply(
+          dataSource,
+          dialect,
+          Blocker.liftExecutionContext(executionContext),
+          limited = false
+        ))
+      }
     } yield {
       transactor
     }
