81632239054458b3c272d888f3389dff6e734e04
zakolenko <zakolenkoroman@gmail.com>
2019-11-22 01:06:58 +0200

    ResultQueryOps.collectTo

--- core/src/main/scala/com/zakolenko/jooq4s/ResultQuery.scala
+++ core/src/main/scala/com/zakolenko/jooq4s/ResultQuery.scala
@@ -1,16 +1,24 @@
 package com.zakolenko.jooq4s
 
-import org.jooq.{Record, ResultQuery}
+import org.jooq.{Record, ResultQuery => JResultQuery}
+
+import scala.collection.generic.CanBuildFrom
 
 object ResultQuery {
 
-  final class Ops[R <: Record](private val rq: ResultQuery[R]) extends AnyVal {
+  final class Ops[R <: Record](private val rq: JResultQuery[R]) extends AnyVal {
     def one[F[_]](tr: Transactor[F]): F[R] = tr.one(rq)
     def option[F[_]](tr: Transactor[F]): F[Option[R]] = tr.option(rq)
+    def collectTo[CC[_]]: CollectTo[CC, R] = new CollectTo(rq)
     def stream[F[_]](tr: Transactor[F]): fs2.Stream[F, R] = tr.stream(rq)
+    def execute[F[_]](tr: Transactor[F]): F[Int] = tr.execute(rq)
+  }
+
+  final class CollectTo[CC[_], R <: Record](private val rq: JResultQuery[R]) extends AnyVal {
+    def apply[F[_]](tr: Transactor[F])(implicit cbf: CanBuildFrom[Nothing, R, CC[R]]): F[CC[R]] = tr.collect(rq)
   }
 
   trait ToResultQueryOps {
-    implicit def toResultQueryOps[R <: Record](rq: ResultQuery[R]): ResultQuery.Ops[R] = new ResultQuery.Ops(rq)
+    implicit def toResultQueryOps[R <: Record](rq: JResultQuery[R]): ResultQuery.Ops[R] = new ResultQuery.Ops(rq)
   }
 }
