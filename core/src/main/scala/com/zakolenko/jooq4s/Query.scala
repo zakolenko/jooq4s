be429dd892f2a08372cb0318824610a2013c2e61
zakolenko <zakolenkoroman@gmail.com>
2019-11-24 23:15:08 +0200

    fix warning

--- core/src/main/scala/com/zakolenko/jooq4s/Query.scala
+++ core/src/main/scala/com/zakolenko/jooq4s/Query.scala
@@ -1,14 +1,14 @@
 package com.zakolenko.jooq4s
 
-import org.jooq.Query
+import org.jooq.{Query => JQuery}
 
 object Query {
 
-  final class Ops(private val query: Query) extends AnyVal {
-    def execute[F[_]](tr: Transactor[F]): F[Int] = tr.execute(query)
+  trait ToQueryOps {
+    implicit def toQueryOps(query: JQuery): Query.Ops = new Query.Ops(query)
   }
 
-  trait ToQueryOps {
-    implicit def toQueryOps(query: Query): Query.Ops = new Query.Ops(query)
+  final class Ops(private val query: JQuery) extends AnyVal {
+    def execute[F[_]](tr: Transactor[F]): F[Int] = tr.execute(query)
   }
 }
